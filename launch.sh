c96c8dc9411c16e684bbfd9aa17e19ba33107fce
m <m@metaquanta.com>
2020-06-15 04:51:15 -0400

    fixed launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/sbin/soft_fn `find /sys/devices/platform/i8042/ -regex .*/event[0-9]`
+/usr/sbin/soft_fn /dev/input/`\
+		find /sys/devices/platform/i8042/ -regex .*/event[0-9]* |\
+	       	sed 's/^.*\(event[0-9]*\).*$/\1/'`
