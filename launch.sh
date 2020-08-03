75a1266dfb494b7b5f297befdd386bd7f9eeccc0
m <m@metaquanta.com>
2020-08-03 01:57:44 -0400

    exec soft-fn

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/sbin/soft_fn /dev/input/`\
+exec /usr/sbin/soft_fn /dev/input/`\
 		find /sys/devices/platform/i8042/ -regex .*/event[0-9]* |\
 	       	sed 's/^.*\(event[0-9]*\).*$/\1/'`
